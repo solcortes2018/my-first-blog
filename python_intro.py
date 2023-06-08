76ce4e704fcc1ec460650b46d43f8bf5a4133e92
solcortes2018 <solecito-2109@hotmail.com>
2023-06-08 14:24:56 -0600

    Mi aplicación Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,40 @@
+"""if 3 > 2:
+     print('It works!')
+
+if 5 > 6:
+    print('5 is indeed greater than 2')
+else:
+    print('5 is not greater than 2')"""
+
+"""name = 'Ola'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')"""
+
+"""volume = 115
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("Me duelen las orejas! :(")"""
+
+"""def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')"""
+
+for i in range(3, 8):
+    print(i)
